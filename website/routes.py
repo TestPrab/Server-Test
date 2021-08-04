0f318d3f3c44c4bc3e6ffc379e8b4ce3269145ad
TestPrab <singh01prabhat@gmail.com>
2021-08-04 12:07:16 +0530

    changed structure

--- website/routes.py
+++ website/routes.py
@@ -1,27 +1,28 @@
-from website import app
-from flask import render_template, redirect, url_for, flash, request, jsonify
-from website.models import User
+from flask.blueprints import Blueprint
+from flask import render_template, redirect, url_for, flash, request
 from website.forms import RegisterForm, LoginForm, OauthForm
-from website import db
 from flask_login import login_user, logout_user, login_required, current_user
-from authlib.integrations.flask_oauth2 import current_token
-from authlib.oauth2 import OAuth2Error
-from .models import db, User, OAuth2Client
-from .oauth2 import authorization, require_oauth
+from .models import User, OAuth2Client, OAuth2AuthorizationCode, OAuth2Token
+import base64
 from werkzeug.security import gen_salt
 import time
+import urllib
+import json
+from bcfserver import db
+
+website = Blueprint("website", __name__, template_folder="templates")
 
 
 def split_by_crlf(s):
     return [v for v in s.splitlines() if v]
 
 
-@app.route("/")
+@website.route("/")
 def homepage():
     return render_template("index.html")
 
 
-@app.route("/register", methods=["GET", "POST"])
+@website.route("/register", methods=["GET", "POST"])
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -48,9 +49,16 @@ def register_page():
     return render_template("register.html", form=form)
 
 
-@app.route("/createclient", methods=["GET", "POST"])
+@website.route("/createclient", methods=["GET", "POST"])
 @login_required
 def create_client():
+    grants = [
+        "AuthorizationCodeGrant",
+        "ImplicitGrant",
+        "ResourceOwnerPasswordCredentialsGrant",
+        "ClientCredentialsGrant",
+        "RefreshTokenGrant",
+    ]
     form = OauthForm()
     if form.validate_on_submit():
         client_id = gen_salt(24)
@@ -69,23 +77,23 @@ def create_client():
             "token_endpoint_auth_method": "client_secret_basic",
         }
         client.client_secret = gen_salt(48)
+
         client.set_client_metadata(client_metadata)
         db.session.add(client)
         db.session.commit()
         flash(
-            "Oaut Client Created Successfully",
+            "Oauth Client Created Successfully",
             category="info",
         )
         clients = OAuth2Client.query.filter_by(user_id=current_user.id).all()
         for client in clients:
             print(client.client_info)
             print(client.client_metadata)
-
         return render_template("clientdata.html", user=current_user.id, clients=clients)
-    return render_template("createclient.html", form=form)
+    return render_template("createclient.html", form=form, grants=grants)
 
 
-@app.route("/login", methods=["GET", "POST"])
+@website.route("/login", methods=["GET", "POST"])
 def login_page():
     form = LoginForm()
     if form.validate_on_submit():
@@ -104,43 +112,184 @@ def login_page():
     return render_template("login.html", form=form)
 
 
-@app.route("/logout")
+@website.route("/logout")
 def logoutpage():
     logout_user()
     flash("You have been logged out!", category="info")
     return redirect(url_for("homepage"))
 
 
-@app.route("/oauth/authorize", methods=["GET", "POST"])
-@login_required
+@website.route("/foundation/1.0/auth")
+def foundation_auth():
+    data = {
+        "oauth2_auth_url": "http://127.0.0.1:5000/oauth/authorize",
+        "oauth2_token_url": "http://127.0.0.1:5000/oauth/token",
+        "supported_oauth2_flows": ["authorization_code"],
+    }
+    response = website @ website.response_class(
+        response=json.dumps(data), status=200, mimetype="website@websitelication/json"
+    )
+    return response
+
+
+@website.route("/foundation/versions")
+def foundation_versions():
+    Body = {
+        "versions": [
+            {
+                "api_id": "opencde-foundation",
+                "version_id": "1.0",
+                "detailed_version": "https://github.com/BuildingSMART/opencde-foundation-API/tree/v1.0",
+            },
+            # {
+            #     "api_id": "bcf",
+            #     "version_id": "2.1",
+            #     "detailed_version": "https://github.com/buildingSMART/BCF-API/tree/release_2_1",
+            #     "api_base_url": "http://127.0.0.1:5000/bcf/2.1"
+            # },
+            {
+                "api_id": "bcf",
+                "version_id": "3.0",
+                "detailed_version": "https://github.com/buildingSMART/BCF-API/tree/release_3_0",
+                "api_base_url": "http://127.0.0.1:5000/bcf/3.0",
+            },
+        ]
+    }
+    response = website @ website.response_class(
+        response=json.dumps(Body), status=200, mimetype="website@websitelication/json"
+    )
+    return response
+
+
+@website.route("/outh/login", methods=["GET", "POST"])
+def oauth_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password(
+            attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(
+                "Invalid Credentials",
+                category="info",
+            )
+            client_id = request.args.get("client_id")
+            redirect_uri = request.args.get("redirect_uri")
+            state = request.args.get("state")
+            return redirect(
+                url_for(
+                    "authorize",
+                    client_id=client_id,
+                    redirect_uri=redirect_uri,
+                    state=state,
+                )
+            )
+        else:
+            flash(
+                "Invalid Credentials",
+                category="danger",
+            )
+    return render_template("ologin.html", form=form)
+
+
+@website.route("/oauth/authorize", methods=["GET", "POST"])
 def authorize():
-    user = current_user.id
-    if not user:
+    client_id = request.args.get("client_id")
+    redirect_uri = request.args.get("redirect_uri")
+    state = request.args.get("state")
+    if current_user.is_anonymous:
         flash("Please Login !", category="info")
-        return redirect(url_for("loginpage"))
-    if request.method == "GET":
-        try:
-            grant = authorization.validate_consent_request(end_user=user)
-        except OAuth2Error as error:
-            return error.error
-        return render_template("authorize.html", user=user, grant=grant)
-    if not user and "username" in request.form:
-        username = request.form.get("username")
-        user = User.query.filter_by(username=username).first()
-    if request.form["confirm"]:
-        grant_user = user
+        query = request.query_string
+        return redirect(
+            url_for(
+                "oauth_login",
+                client_id=client_id,
+                redirect_uri=redirect_uri,
+                state=state,
+            )
+        )
     else:
-        grant_user = None
-    return authorization.create_authorization_response(grant_user=grant_user)
+        try:
+
+            user = current_user.id
+            flash(message=redirect_uri, category="warning")
+            client = OAuth2Client.query.filter_by(client_id=client_id).first()
+            if client:
+                if client.user_id == user:
+                    code = gen_salt(48)
+                    OauthCode = OAuth2AuthorizationCode(
+                        client_id=client_id,
+                        redirect_uri=redirect_uri,
+                        user_id=user,
+                        code=code,
+                        response_type="code",
+                    )
+                    db.session.add(OauthCode)
+                    db.session.commit()
+                    query = urllib.parse.urlencode(
+                        {
+                            "code": OauthCode.code,
+                            "state": state,
+                        }
+                    )
+                    return redirect(f"{redirect_uri}?{query}")
+                else:
+                    flash(
+                        "You are not authorized to access this client",
+                        category="danger",
+                    )
+            else:
+                flash("Client not found", category="danger")
+        except Exception as e:
+            flash(e, category="danger")
+    return render_template("oauth.html")
 
 
-@app.route("/oauth/token", methods=["POST"])
+@website.route("/oauth/token", methods=["POST"])
 def issue_token():
-    return authorization.create_token_response()
-
-
-@app.route("/api/me")
-@require_oauth("profile")
-def api_me():
-    user = current_token.user
-    return jsonify(id=user.id, username=user.username)
+    try:
+        code = request.form["code"]
+        Headers = str.split(request.headers["Authorization"])
+        decode_header = base64.b64decode(Headers[1]).decode("utf-8")
+        creds = decode_header.split(":")
+        client_id = creds[0]
+        auth_code = OAuth2AuthorizationCode.query.filter_by(code=code).first()
+        if auth_code:
+            if auth_code.client_id == client_id:
+                access_token = gen_salt(48)
+                refresh_token = gen_salt(48)
+                expires_in = 3600
+                OauthToken = OAuth2Token(
+                    client_id=auth_code.client_id,
+                    user_id=auth_code.user_id,
+                    access_token=access_token,
+                    refresh_token=refresh_token,
+                    expires_in=expires_in,
+                    scope=auth_code.scope,
+                    token_type="Bearer",
+                )
+                db.session.add(OauthToken)
+                db.session.commit()
+                query = {
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "expires_in": expires_in,
+                    "auth_code": auth_code.scope,
+                }
+                response = website @ website.response_class(
+                    response=json.dumps(query),
+                    status=200,
+                    mimetype="website@websitelication/json",
+                )
+                return response
+            else:
+                flash(
+                    "You are not authorized to access this client",
+                    category="danger",
+                )
+        else:
+            flash("Client not found", category="danger")
+    except Exception as e:
+        flash(e, category="danger")
+    return render_template("oauth.html")
